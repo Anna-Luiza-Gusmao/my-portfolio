146afa6680670bb6010bb8fae6c57a7faa08fe06
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2023-01-16 20:17:34 -0300

    AddThemes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
 import { BrowserRouter } from 'react-router-dom'
 import { Routers } from './Routers'
 
 export function App() {
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
           <Routers />
-    </BrowserRouter>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+    'white': '#FAFAFA',
+    'white-100': '#FFF6F6',
+    'white-200': '#F3F2F2',
+
+    'orange-400': '#FFAE35',
+    'orange-500': '#E78D07',
+
+    'gray-400': '#504A49',
+    'gray-500': '#332F2E',
+    'gray-700': '#181616',
+    'gray-800': '#1A1A1A'
+}
