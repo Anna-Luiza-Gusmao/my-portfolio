d36fc80bd54bf86b922dfd193717c583bd70d234
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2023-01-19 20:10:24 -0300

    PageHome v1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,32 @@
+import { Header, HomeContainer, TextContainer } from "./styles"
+import LinkedinIcon from '../../assets/icons/linkedin-icon.svg'
+import GithubIcon from '../../assets/icons/github-icon.svg'
+import { ButtonNextPage } from "../../components/ButtonNextPage"
+import { Carousel } from "./components/Carrousel"
+
 export function Home () {
+    const navigateFromLinkedin = () => {
+        window.location.href = 'https://www.linkedin.com/in/anna-luiza-gusm%C3%A3o-b248b3216/'
+    }
+    const navigateFromGithub = () => {
+        window.location.href = 'https://github.com/Anna-Luiza-Gusmao'
+    }
+
     return (
-        <h1>Page Home</h1>
+        <HomeContainer>
+            <Header>
+                <img src={LinkedinIcon} onClick={navigateFromLinkedin} />
+                <img src={GithubIcon} onClick={navigateFromGithub} />
+            </Header>
+            <TextContainer>
+                <h1>Hi!</h1>
+                <h1>I am Anna Luiza</h1>
+                <h2>| FRONTEND DEVELOPER |</h2>
+            </TextContainer>
+            <Carousel />
+            <footer style={{'right': '0', 'position': 'absolute', 'margin': '3rem 0'}}>
+                <ButtonNextPage path="about-me" />
+            </footer>
+        </HomeContainer>
     )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+
+export const HomeContainer = styled.main`
+    background: ${props => props.theme["white"]};
+    width: 100%;
+    height: 100vh;
+`
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    padding: 1.25rem;
+
+    img {
+        transition: box-shadow 0.3s;
+        
+        &:hover{
+            cursor: pointer;
+            box-shadow: 0 4px 14px 0 rgba(51, 47, 46, 0.25);
+        }
+    }
+`
+
+export const TextContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+
+    h1 {
+        padding: 1rem 0;
+        color: ${props => props.theme["gray-700"]};
+        font-size: 4rem;
+
+        :nth-child(2) {
+            font-size: 3rem;
+        }
+    }
+
+    h2 {
+        font-size: 4rem;
+        padding: 4.5rem 0;
+        color: ${props => props.theme["orange-500"]};
+    }
+`
