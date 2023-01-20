7b25aaa49081692a7127927bccf505a7d1c67465
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2023-01-19 23:25:49 -0300

    AddStacksComponent

--- src/pages/AboutMe/components/Stacks/index.tsx
+++ src/pages/AboutMe/components/Stacks/index.tsx
@@ -1,8 +1,31 @@
-import { StacksContainer } from "./styles"
+import { OrangeStack, GrayStack, StacksContainer, AllStacks } from "./styles"
 
-export function Stacks () {
+export function Stacks() {
+    const stacks = [
+        'Bootstrap',
+        'Insomnia',
+        'React Router',
+        'React Hooks',
+        'Styled Components',
+        'Figma',
+        'Json Server',
+        'MUI',
+        'Git'
+    ]
+
+
     return (
         <StacksContainer>
+            <AllStacks>
+                {
+                    stacks.map((item) => (
+                        <div style={{ 'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center' }}>
+                            <OrangeStack />
+                            <GrayStack>{item}</GrayStack>
+                        </div>
+                    ))
+                }
+            </AllStacks>
         </StacksContainer>
     )
 } 
--- src/pages/AboutMe/components/Stacks/styles.ts
+++ src/pages/AboutMe/components/Stacks/styles.ts
@@ -8,3 +8,35 @@ export const StacksContainer = styled.div`
     padding: 11.5rem 0;
     margin-bottom: 4.75rem;
 `
+
+export const AllStacks = styled.section`
+    display: grid;
+    justify-items: center;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    row-gap: 3.25rem;
+    column-gap: 2.75rem;
+`
+
+export const OrangeStack = styled.div`
+    width: 230px;
+    height: 53px;
+    background: ${props => props.theme["orange-400"]};
+    border-radius: 38px;
+`
+
+export const GrayStack = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 198px;
+    height: 50px;
+    margin-top: -4rem;
+
+    background: ${props => props.theme["gray-500"]};
+    border-radius: 18px;
+    border: 1px solid ${props => props.theme["orange-400"]};
+
+    color: ${props => props.theme["orange-400"]};
+    font-family: 'Inter', sans-serif;
+    font-size: 1.25rem;
+`
